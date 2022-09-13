0c5e260f5c84d54f2f551d21bf4d9c2ede8875c4
Pedro Henrique <system.pedrohenrique@gmail.com>
2022-09-13 13:53:55 -0300

    entc/gen: adds test for issue 2761 and generate

--- entc/integration/ulid/ent/schema/user.go
+++ entc/integration/ulid/ent/schema/user.go
@@ -2,12 +2,25 @@ package schema
 
 import (
 	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
 	"github.com/ent/ent/entc/integration/ulid/ent/schema/mixin"
 )
+
+type OccupancyPricing struct {
+	Price int
+}
 
 // User holds the schema definition for the User entity.
 type User struct {
 	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		// issue: 2761
+		field.JSON("occupancy_pricing", map[string]OccupancyPricing{}),
+	}
 }
 
 // Mixin of the User.
